ad1480f12720c892694c575c07ba30f4b0cb2976
Jorge Ossa <jorgehossa@gmail.com>
2020-06-12 01:09:55 -0500

    Split MediaPlayer class to external module

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,25 +1,7 @@
+import MediaPlayer from './MediaPlayer.js';
+
 const video = document.querySelector('video');
-  const button = document.querySelector('button');
+const player = new MediaPlayer({ el: video });
 
-  function MediaPlayer(config) {
-    this.media = config.el;
-  }
-
-  MediaPlayer.prototype.play = function () {
-    this.media.play();
-  }
-
-  MediaPlayer.prototype.pause = function () {
-    this.media.pause();
-  }
-
-  MediaPlayer.prototype.togglePlay = function () {
-    if (this.media.paused) {
-      this.play();
-    } else {
-      this.pause();
-    }
-  }
-
-  const player = new MediaPlayer({ el: video });
-  button.onclick = () => player.togglePlay();
+const button = document.querySelector('button');
+button.onclick = () => player.togglePlay();

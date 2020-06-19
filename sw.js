6dfdd382faf1a8ac8849119cf1884a7207c0e08f
Jorge Ossa <jorgehossa@gmail.com>
2020-06-18 21:34:21 -0500

    Convert all project to TypeScript and add parcel

--- sw.js
+++ sw.js
@@ -1,44 +1,44 @@
 const VERSION = 'v1';
 
 self.addEventListener('install', event => {
-  event.waitUntil(precache())
-})
+  event.waitUntil(precache());
+});
 
 self.addEventListener('fetch', event => {
   const request = event.request;
   // get
-  if (request.method == 'GET') {
+  if (request.method !== 'GET') {
     return;
   }
 
-  // search in cache
-  event.respodWith(cachedResponse(request))
+  // buscar en cache
+  event.respondWith(cachedResponse(request));
 
-  //
-  event.waitUntil(updateCache(request))
-})
+  // actualizar el cache
+  event.waitUntil(updateCache(request));
+});
 
-async function precache () {
-  const cache = await caches.open(VERSION)
+async function precache() {
+  const cache = await caches.open(VERSION);
   return cache.addAll([
-    '/',
-    '/src/index.html',
-    '/src/assets/index.js',
-    '/src/assets/index.css',
-    '/src/assets/MediaPlayer.js',
-    '/src/assets/plugins/AutoPlay.js',
-    '/src/assets/plugins/AutoPause.js',
-    '/src/assets/spring-blender-open-movie.mp4'
-  ])
+    /* '/',
+    '/index.html',
+    '/assets/index.js',
+    '/assets/MediaPlayer.js',
+    '/assets/plugins/AutoPlay.js',
+    '/assets/plugins/AutoPause.js',
+    '/assets/index.css',
+    '/assets/BigBuckBunny.mp4', */
+  ]);
 }
 
-async function cachedResponse (request) {
+async function cachedResponse(request) {
   const cache = await caches.open(VERSION);
   const response = await cache.match(request);
   return response || fetch(request);
 }
 
-async function updateCache (request) {
+async function updateCache(request) {
   const cache = await caches.open(VERSION);
   const response = await fetch(request);
   return cache.put(request, response);
